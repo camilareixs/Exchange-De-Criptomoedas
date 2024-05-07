73322228ee9b103b46ea193624798b4a9ac70100
user <user@10.210.29.223>
2024-05-07 20:04:05 -0300

    Menu

---
+++ src/controller/Controller.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.janelaLogin;
+
+/**
+ *
+ * @author user
+ */
+public class Controller {
+    private janelaLogin login;
+    private String cpf;
+    private int senha;
+
+    public janelaLogin getLogin() {
+        return login;
+    }
+
+    public void setLogin(janelaLogin login) {
+        this.login = login;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    
+    
+    
+    
+    
+}
---
+++ src/projetocripto/ProjetoCripto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetocripto;
+
+import view.janelaLogin;
+
+/**
+ * 01/05/2024
+ * @author camilareixs
+ */
+public class ProjetoCripto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        janelaLogin login = new janelaLogin();
+        login.setVisible(true);
+        
+        
+    }
+    
+}
